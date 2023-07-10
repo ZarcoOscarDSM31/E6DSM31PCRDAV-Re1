d7c7fa594a91f9367c2e1bedbcacb5096e813bd9
JuanDiDSM31 <al222111490@gmail.com>
2023-07-10 09:30:19 -0600

    layout

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    hola layout dsm-31
-</body>
-</html>
+<?php
+include "layout/menu.php";
+include "layout/header.php";
+include "layout/footer.php";
+?>
---
+++ views/Products/index.php
@@ -0,0 +1,6 @@
+<?php
+include ("./../../layout/menu.php");
+include ("./../../layout/header.php");
+echo "<p>hola desde productos</p>";
+include ("./../../layout/footer.php");
+?>
